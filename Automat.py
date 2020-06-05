2d58c0264405db284407722678caa69bcd9d1d06
bziembanski <bziembanski@gmail.com>
2020-06-05 22:50:40 +0200

    Wydawanie reszty metoda dynamiczna i obsluga zaplaty

--- Automat.py
+++ Automat.py
@@ -6,34 +6,26 @@ LISTA_NOMINALOW = [1, 2, 5, 10, 20, 50,
 
 class Bilet:
     def __init__(self, dlugosc, typ, cena):
-        self._dlugosc = dlugosc
-        self._typ = typ
-        self._cena = cena
+        self.dlugosc = dlugosc
+        self.typ = typ
+        self.cena = cena
 
     def wyswietl_bilet(self):
-        print(f"Bilet {self._typ} {self._dlugosc}-minutowy - {self._cena / 100}")
-
-    def get_dlugosc(self):
-        return self._dlugosc
-
-    def get_typ(self):
-        return self._typ
-
-    def get_cena(self):
-        return self._cena
+        print(f"Bilet {self.typ} {self.dlugosc}-minutowy - {self.cena / 100}")
 
     def __eq__(self, other):
-        return self._dlugosc == other._dlugosc and self._typ == other._typ and self._cena == other._cena
+        return self.dlugosc == other.dlugosc and self.typ == other.typ and self.cena == other.cena
 
     def __repr__(self):
-        return f'Bilet({self._dlugosc}, "{self._typ}", {self._cena})'
+        return f'Bilet({self.dlugosc}, "{self.typ}", {self.cena})'
 
 
 class Automat:
     def __init__(self, nominaly):
-        self._zawartosc = {x: 0 for x in nominaly}
+        self._nominaly = nominaly
+        self._zawartosc = {x: 0 for x in self._nominaly}
         self._bilety = []
-        self._wrzucone = {x: 0 for x in nominaly}
+        self._wrzucone = {x: 0 for x in self._nominaly}
 
     def wrzuc_monety(self, moneta, ilosc):
         if moneta in self._wrzucone:
@@ -46,9 +38,7 @@ class Automat:
             self._zawartosc[i] = ile
 
     def pokaz_monety(self, wrzucone=False):
-        dic = self._zawartosc
-        if wrzucone:
-            dic = self._wrzucone
+        dic = self._wrzucone if wrzucone else self._zawartosc
         for i, j in dic.items():
             print(f'{i / 100}zł\t{j}')
 
@@ -66,48 +56,81 @@ class Automat:
             suma += nominal * ilosc
         return suma
 
-    def wydaj_reszte(self, kwota):
-        do_wydania = int(kwota * 100)  # zamiana na grosze
-        if self.suma_zawartosc() < kwota:
-            return {}
-        nominaly = [*self._zawartosc]
-        nominaly.reverse()
-        # print(nominaly)
-        reszta = {}
-        i = 0
-        while do_wydania > 0 and i < len(nominaly):
-            if do_wydania >= nominaly[i]:
-                ile = floor(do_wydania / nominaly[i])
-                if ile > self._zawartosc[nominaly[i]]:
-                    ile = self._zawartosc[nominaly[i]]
-                do_wydania = round(100 * (do_wydania - (nominaly[i] * ile))) / 100
-                reszta[nominaly[i]] = ile
-                self._zawartosc[nominaly[i]] -= ile
-            i += 1
-        return reszta
-
-    def dodaj_bilet(self, bilet):
-        self._bilety.append(bilet)
+    def dodaj_bilet(self, bilet, ile=1):
+        for _ in range(ile):
+            self._bilety.append(bilet)
 
     def usun_bilet(self, bilet):
         if bilet in self._bilety:
             self._bilety.remove(bilet)
 
     def do_zaplaty(self):
-        return sum(bilet.get_cena() for bilet in self._bilety)
+        return sum(bilet.cena for bilet in self._bilety)
+
+    def czy_moge_wydac(self):
+        max_i = 999999999
+        coins_used = []
+        min_coins = []
+        limit = list(self._zawartosc.values())
+        coins = list(self._zawartosc.keys())
+        reszta = self.suma_monet() - self.do_zaplaty()
+        for i in range(reszta + 1):
+            coins_used.append([])
+            for j in range(len(self._zawartosc.items())):
+                coins_used[i].append(0)
+        min_coins.append(0)
+        for i in range(reszta):
+            min_coins.append(max_i)
+        for i in range(len(coins)):
+            while limit[i] > 0:
+                for j in reversed(range(reszta)):
+                    curr_amount = j + coins[i]
+                    if curr_amount <= reszta:
+                        if min_coins[curr_amount] > min_coins[j] + 1:
+                            min_coins[curr_amount] = min_coins[j] + 1
+                            coins_used[curr_amount] = coins_used[j].copy()
+                            coins_used[curr_amount][i] += 1
+                limit[i] -= 1
+        if min_coins[reszta] == max_i:
+            return 0, {x: 0 for x in self._nominaly}
+        return min_coins[reszta], dict(zip(coins, coins_used[reszta]))
+
+    def zaplac(self):
+        if self.suma_monet() < self.do_zaplaty():
+            print("Wprowadzono niepełną kwotę!")
+            return {}
+        reszta = self.suma_monet() - self.do_zaplaty()
+        if reszta == 0:
+            for nominal, ilosc in self._wrzucone.items():
+                self._zawartosc[nominal] += ilosc
+            bilety = self._bilety.copy()
+            self._bilety = []
+            return {}
+        else:
+            czy_moge, reszta_dict = self.czy_moge_wydac()
+            if czy_moge == 0:
+                print("Tylko odliczona kwota")
+                wrzucone = self._wrzucone.copy()
+                self._wrzucone = {x: 0 for x in self._nominaly}
+                return wrzucone
+            else:
+                for n in self._nominaly:
+                    self._zawartosc[n] += self._wrzucone[n]
+                    self._zawartosc[n] -= reszta_dict[n]
+                self._bilety = []
+                return reszta_dict
 
 
 def main():
     a = Automat(LISTA_NOMINALOW)
-    a.napelnij(10)
-    a.pokaz_monety()
-    r = a.wydaj_reszte(6.99)
-    print(a.suma_monet(r))
-    a.pokaz_monety()
+    a.napelnij(1)
 
     a.dodaj_bilet(Bilet(50, 'normalny', 400))
-    a.dodaj_bilet(Bilet(30, 'normalny', 300))
-    a.wrzuc_monety(500, 2)
+    a.dodaj_bilet(Bilet(30, 'normalny', 159))
+    a.wrzuc_monety(500, 1)
+    a.wrzuc_monety(50, 1)
+    a.wrzuc_monety(5, 2)
+    print(a.zaplac())
 
 
 if __name__ == '__main__':
